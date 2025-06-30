212a2f2025db5312a7bf44ee5b3586d60996b828
CultureGod <dillatheking@gmail.com>
2025-06-30 04:54:23 +0300

    обновлен masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,8 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
     card_number = str(card_number)
-
+    if len(card_number) < 8:
+        return ""
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
